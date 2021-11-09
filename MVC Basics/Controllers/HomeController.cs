d97c8a2a8bdf638a594b900b58df90baafdfc9d8
Stefan Sundbeck <stefan.sundbeck@wpnet.se>
2021-11-09 14:59:12 +0100

    Add project files.

---
+++ MVC Basics/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Test.Controllers
+{
+    public class HomeController : Controller
+    {
+	public IActionResult Index()
+	{
+	    return View();
+	}
+
+	public IActionResult About()
+	{
+	    return View();
+	}
+
+	public IActionResult Projects()
+	{
+	    return View();
+	}
+
+	public IActionResult Contact()
+	{
+	    return View();
+	}
+    }
+}

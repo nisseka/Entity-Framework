1fe17de69e47c5e3ec59a2a975b405eecee6eeae
Stefan Sundbeck <stefan.sundbeck@wpnet.se>
2021-11-09 15:07:07 +0100

    Corrected namespace name of controller

--- MVC Basics/Controllers/HomeController.cs
+++ MVC Basics/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ASP.NET_Test.Controllers
+namespace MVC_Basics.Controllers
 {
     public class HomeController : Controller
     {
